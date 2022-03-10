4ba47bf28ec2a78f1b412e455e2a4eacceaac2a9
NazrinRahimli <100777306+NazrinRahimli@users.noreply.github.com>
2022-03-10 17:03:08 +0400

    Add files via upload

---
+++ layihe #1/index.js
@@ -0,0 +1,9 @@
+function funk() { 
+    document.getElementById("button").style.display="block"; 
+    document.getElementById("button2").style.display="none"; 
+} 
+ 
+function funk2() { 
+    document.getElementById("button2").style.display="block"; 
+    document.getElementById("button").style.display="none"; 
+} 
